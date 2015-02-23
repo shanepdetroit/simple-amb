65fae1ac988519eb2b5c48db01b489243d89a882
shanepdetroit <shanepdetroit@gmail.com>
2015-02-23 17:39:54 -0500

    Update forward.sh

--- forward.sh
+++ forward.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-trap "shut_down" SIGKILL SIGTERM SIGHUP SIGINT EXIT
-
 shut_down(){
   printf "%s\n" "Caught SIGTERM signal!" 
   kill -TERM $child 2>/dev/null
 }
+
+trap shut_down SIGKILL SIGTERM SIGHUP SIGINT EXIT
 
 ADDR=${1:-$FORWARD_ADDR}
 exec socat TCP-LISTEN:10000,fork TCP:$ADDR
